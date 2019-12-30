6ec3ac5c83e96d40727dd7e4630c8fccc8adb7b5
KEATON TATOOLES <keaton.tatooles@ibm.com@KEATONs-MacBook-Pro.local>
2019-12-30 11:07:45 -0500

    Returned to mock service

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import UIShell from "./components/UIShell";
 import "./App.scss";
 import {StockItemService} from './services/stock-item.service';
+import {StockItemMockService} from './services/stock-item-mock.service';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.stockService = props.stockService || new StockItemService();
+    this.stockService = props.stockService || new StockItemMockService();
   }
   render() {
     return (
